5be85abb9263990f8e2fcf97d3ade0d3587dc792
Manoj <YOUR_GITHUB_EMAIL>
2026-08-27 21:18:05 +0530

    Add About page and improve report navigation

--- app.py
+++ app.py
@@ -39,6 +39,9 @@ def create_database():
 @app.route("/")
 def home():
     return render_template("home.html")
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 
 @app.route("/register", methods=["GET", "POST"])
